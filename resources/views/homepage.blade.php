8d0116972cf72b355529f50bef62e3c8c41b8d4b
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-08-26 16:37:33 +0200

    add styling page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+{{-- @extends ('layout')
+
+
+@section ('link')
+    <div class="container-link">
+        <a href="view"> ADD VAMPIRES TO GET BLOOD 🩸 </a>
+        <br>
+        <a href="database"> Transylvania 🧛🏻🧛🏼🧛🏽🧛🏾🧛🏿🧛‍♂️🧛‍♀️</a>
+    </div>
+@endsection --}}
